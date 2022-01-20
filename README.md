583bf034518409fdef0031c58104af728188f6cd
Kylan1940 <Kevingaming123copyright@gmail.com>
2022-01-20 15:59:57 +0700

    little changes

--- README.md
+++ README.md
@@ -3,34 +3,37 @@
 Heal and Feed Command For PocketMine-MP. If you are have a question, please make a [issues](https://github.com/Kylan1940/HealAndFeed/issues/new)
 
 # Changelogs [2.1.0]
->- Added HealAndFeed UI
+>- Console Support
+>- Minimum Health or Hungry to use the commands
 
 # Features
 >- Simple
 >- Configuration
 >- Can heal and feed other player
 >- With UI
+>- Console Support
+>- Minimum Health or Hungry to use commands
 >- Pay for heal and feed (soon)
->- Minimum Health or Hungry to use command (soon)
 
 # Download
 | Version | Phar | Support |
 |---|---|---|
+| 2.2.0 |  [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.2.0/HealAndFeed_v2.2.0.phar) |  4.x.x |
 | 2.1.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.1.0.phar) |  4.x.x |
 | 2.0.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.0.0/HealAndFeed_v2.0.0.phar) |  4.x.x |
 | 1.1.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/1.1.0/HealAndFeed_v1.1.0.phar) |  3.x.x |
 | 1.0.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/1.0.0/HealAndFeed_v1.0.0.phar) |  3.x.x |
 
 # Installation
-1. Download the [plugin](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.1.0.phar)
+1. Download the [plugin](https://github.com/Kylan1940/HealAndFeed/releases/download/2.2.0/HealAndFeed_v2.2.0.phar)
 2. Put plugin to **plugins** file
 3. Restart the server
 
 # Permission
-| Command | Permission | Default | Description |
+| Command | Permission | Default | Console Support|
 |---|---|---|---|
-| /healfeed | healandfeed-ui.command | OP | HealAndFeed UI |
-| /heal | healandfeed-heal.command | OP | Heal Command |
-| /heal (PlayerName) | healandfeed-heal.command | OP | Heal Other Player |
-| /feed | healandfeed-feed.command | OP | Feed Command |
-| /feed (PlayerName) | healandfeed-feed.command | OP | Feed Other Player | 
+| /healfeed | healandfeed-ui.command | OP | NO |
+| /heal | healandfeed-heal.command | OP | NO |
+| /heal (PlayerName) | healandfeed-heal.command | OP | YES |
+| /feed | healandfeed-feed.command | OP | NO |
+| /feed (PlayerName) | healandfeed-feed.command | OP | YES |
--- src/Kylan1940/HealAndFeed/Main.php
+++ src/Kylan1940/HealAndFeed/Main.php
@@ -9,9 +9,10 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
-use Kylan1940\HealAndFeed\Form\{Form, FormAPI, SimpleForm};
+use Kylan1940\HealAndFeed\Form\{Form, SimpleForm};
+use Kylan1940\HealAndFeed\commands\{HealCommand, FeedCommand, UICommand};
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase {
 	
 	public function onEnable() : void {
         $this->saveDefaultConfig();
@@ -19,49 +20,4 @@ class Main extends PluginBase implements Listener{
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
       }
-	
-  public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool{
-           if ($sender instanceof Player) {
-               if ($cmd->getName() == "heal") {
-                       $sender->setHealth($sender->getMaxHealth());
-                       $sender->sendMessage($this->getConfig()->get("message-heal")); 
-               }
-              if ($cmd->getName() == "feed") {
-                       $sender->getHungerManager()->setFood(20);
-                       $sender->getHungerManager()->setSaturation(20);
-                       $sender->sendMessage($this->getConfig()->get("message-feed"));
-              }
-             if($cmd->getName() == "healfeed"){
-                $this->HealFeed($sender);
-              }
-           } else {
-               $sender->sendMessage("§cYou must be in-game to use this command!");
-           }
-          return true;
-		}
-		
-  public function HealFeed($sender){
-        $form = new SimpleForm(function (Player $sender, int $data = null){
-            $result = $data;
-            if ($result === null) {
-                return true;
-            }
-            switch ($result) {
-                case 0:
-                    $sender->setHealth($sender->getMaxHealth());
-                    $sender->sendMessage($this->getConfig()->get("message-heal"));
-                  break;
-                case 1:
-                    $sender->setFood(20);
-                    $sender->setSaturation(20);
-                    $sender->sendMessage($this->getConfig()->get("message-feed"));
-                  break;
-            }
-        });
-            $form->setTitle($this->getConfig()->get("title"));
-            $form->addButton($this->getConfig()->get("button-heal"));
-            $form->addButton($this->getConfig()->get("button-feed"));
-            $form->sendToPlayer($sender);
-            return $form;
-    }
 }
