0ba49a706907131ed82d4303da97b7a20b380d74
Kylan1940 <kevingaming123copyright@gmail.com>
2023-04-02 12:46:31 +0700

    Removed Pay For Heal And Feed

--- README.md
+++ README.md
@@ -3,28 +3,28 @@
 Heal and Feed Command For PocketMine-MP. If you are have a question, please make a [issues](https://github.com/Kylan1940/HealAndFeed/issues/new)
 
 # Changelogs [2.1.0]
->- Added UI
->- Pay for heal and feed
+>- Removed Pay for heal and feed
 
 # Features
 >- Simple
 >- Configuration
 >- Can heal and feed other player
 >- With UI
->- Pay for heal and feed
+>- Pay for heal and feed (soon)
 >- Console Support (soon)
 >- Minimum Health or Hungry to use commands (soon)
 
 # Download
 | Version | Phar | Support |
 |---|---|---|
+| 2.2.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.2.0.phar) |  4.x.x |
 | 2.1.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.1.0.phar) |  4.x.x |
 | 2.0.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/2.0.0/HealAndFeed_v2.0.0.phar) |  4.x.x |
 | 1.1.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/1.1.0/HealAndFeed_v1.1.0.phar) |  3.x.x |
 | 1.0.0 | [Download](https://github.com/Kylan1940/HealAndFeed/releases/download/1.0.0/HealAndFeed_v1.0.0.phar) |  3.x.x |
 
 # Installation
-1. Download the [plugin](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.1.0.phar)
+1. Download the [plugin](https://github.com/Kylan1940/HealAndFeed/releases/download/2.1.0/HealAndFeed_v2.2.0.phar)
 2. Put plugin to **plugins** file
 3. Restart the server
 
--- src/Kylan1940/HealAndFeed/Main.php
+++ src/Kylan1940/HealAndFeed/Main.php
@@ -7,40 +7,40 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
-use onebone\economyapi\EconomyAPI;
 use Kylan1940\HealAndFeed\Form\{Form, SimpleForm};
 
 class Main extends PluginBase implements Listener {
-
-   public function onEnable() : void {
+  
+  public function onEnable() : void {
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
-   }
-
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+       
+  }
+   
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
         if($sender instanceof Player){
-          $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-          $moneyheal = ($this->getConfig()->get("money-heal"));
-          $moneyfeed = ($this->getConfig()->get("money-feed"));
-          $money = $economy->myMoney($sender);
+          //$economy = $this->getServer()->getPluginManager()->getPlugin("BedrockEconomy");
+          //$moneyheal = ($this->getConfig()->get("money-heal"));
+          //$moneyfeed = ($this->getConfig()->get("money-feed"));
+          //$money = $economy->myMoney($sender);
                 if($cmd->getName() == "heal"){
-                  if($money >= $moneyheal){
-                    $economy->reduceMoney($sender, $moneyheal);
+                  //if($money >= $moneyheal){
+                    //$economy->reduceMoney($sender, $moneyheal);
                     $sender->setHealth($sender->getMaxHealth());
                     $sender->sendMessage($this->getConfig()->get("message-heal"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
-                  }
+                  //} else {
+                    //$sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
+                  //}
                 }
                 if($cmd->getName() == "feed"){
-                  if($money >= $moneyfeed){
-                    $economy->reduceMoney($sender, $moneyfeed);
+                  //if($money >= $moneyfeed){
+                    //$economy->reduceMoney($sender, $moneyfeed);
                     $sender->getHungerManager()->setFood(20);
                     $sender->getHungerManager()->setSaturation(20);
                     $sender->sendMessage($this->getConfig()->get("message-feed"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
-                  }
+                  //} else {
+                    //$sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
+                  //}
                 } 
               if($cmd->getName() == "healfeed"){
                 $this->HealFeed($sender);
@@ -59,29 +59,29 @@ class Main extends PluginBase implements Listener {
             }
             switch ($result) {
                 case 0:
-                  $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-                  $money = $economy->myMoney($sender); 
-                  $moneyheal = ($this->getConfig()->get("money-heal"));
-                  if($money >= $moneyheal){
-                    $economy->reduceMoney($sender, $moneyheal);
+                  //$economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+                  //$money = $economy->myMoney($sender); 
+                  //$moneyheal = ($this->getConfig()->get("money-heal"));
+                  //if($money >= $moneyheal){
+                    //$economy->reduceMoney($sender, $moneyheal);
                     $sender->setHealth($sender->getMaxHealth());
                     $sender->sendMessage($this->getConfig()->get("message-heal"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
-                  }
+                  //} else {
+                    //$sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
+                  //}
                   break;
                 case 1:
-                  $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-                  $money = $economy->myMoney($sender);
-                  $moneyfeed = ($this->getConfig()->get("money-feed"));
-                    if($money >= $moneyfeed){
-                    $economy->reduceMoney($sender, $moneyfeed);
+                  //$economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+                  //$money = $economy->myMoney($sender);
+                  //$moneyfeed = ($this->getConfig()->get("money-feed"));
+                    //if($money >= $moneyfeed){
+                    //$economy->reduceMoney($sender, $moneyfeed);
                     $sender->getHungerManager()->setFood(20);
                     $sender->getHungerManager()->setSaturation(20);
                     $sender->sendMessage($this->getConfig()->get("message-feed"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
-                  }
+                  //} else {
+                    //$sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
+                  //}
                   break;
             }
         });
@@ -91,4 +91,5 @@ class Main extends PluginBase implements Listener {
             $form->sendToPlayer($sender);
             return $form;
     }
+
 }
