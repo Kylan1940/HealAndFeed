eb05a9f5ac21a5a9cc613da03159502643381af9
Kylan1940 <kevingaming123copyright@gmail.com>
2023-06-02 23:35:43 +0700

    added ui

--- src/Kylan1940/HealAndFeed/Main.php
+++ src/Kylan1940/HealAndFeed/Main.php
@@ -8,6 +8,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
+use Kylan1940\HealAndFeed\Form\{Form, SimpleForm};
 
 class Main extends PluginBase implements Listener {
   
@@ -15,12 +16,6 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->getResource("config.yml");
-        
-        // Check config
-        if($this->getConfig()->get("config-ver") != 3)
-        {
-            $this->getLogger()->info("HealAndFeed's config is NOT up to date. Please delete the config.yml and restart the server or the plugin may not work properly.");
-        }
   }
    
   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
@@ -42,10 +37,50 @@ class Main extends PluginBase implements Listener {
                     $sender->sendMessage($this->getConfig()->get("no-permission-feed"));
                   }
                 } 
+                if($cmd->getName() == "healfeed"){
+                  if ($sender -> hasPermission("healandfeed-ui.command")) {
+                    $this->HealFeed($sender);
+                  } else {
+                    $sender->sendMessage($this->getConfig()->get("no-permission-ui"));
+                  }
         } else {
           $sender->sendMessage($this->getConfig()->get("only-ingame"));
         }
         return true;
+    }
+  }
+    
+  public function HealFeed($sender){
+        $form = new SimpleForm(function (Player $sender, int $data = null){
+            $result = $data;
+            if ($result === null) {
+                return true;
+            }
+            switch ($result) {
+                case 0:
+                    if ($sender -> hasPermission("healandfeed-heal.command")) {
+                      $sender->setHealth($sender->getMaxHealth());
+                      $sender->sendMessage($this->getConfig()->get("message-heal")); 
+                    } else {
+                      $sender->sendMessage($this->getConfig()->get("no-permission-heal"));
+                    }
+                  break;
+                case 1:
+                    if ($sender -> hasPermission("healandfeed-feed.command")) {
+                      $sender->getHungerManager()->setFood(20);
+                      $sender->getHungerManager()->setSaturation(20);
+                      $sender->sendMessage($this->getConfig()->get("message-feed")); 
+                    } else {
+                      $sender->sendMessage($this->getConfig()->get("no-permission-feed"));
+                    }
+                  break;
+            }
+        });
+            $form->setTitle($this->getConfig()->get("title"));
+            $form->addButton($this->getConfig()->get("button-heal"));
+            $form->addButton($this->getConfig()->get("button-feed"));
+            $form->sendToPlayer($sender);
+            return $form;
     }
 
 }
